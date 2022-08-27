457be062f0001b446b9afd945a296136280454c0
cos <cosogor@gmail.com>
2022-08-27 15:47:02 -0400

    move trach to junk

--- doc/Example Automated Test with Node.js, Selenium, and Jasmine — Skuid v15.1.3 Documentation_files/documentation_options.js
+++
@@ -1,9 +0,0 @@
-var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: '',
-    VERSION: '15.1.3',
-    LANGUAGE: 'en',
-    COLLAPSE_INDEX: false,
-    FILE_SUFFIX: '.html',
-    HAS_SOURCE: true,
-    SOURCELINK_SUFFIX: '.txt'
-};
--- doc/Free Intermediate Node and Express Course _ Rithm School_files/online_course_shared-27e1c62bf9dc9f24b1abb493e3d1bc1dca8b75ce5bff13a91631abe78b40f6f1.js
+++
@@ -1,1 +0,0 @@
-$(function(){$(".cta-btn-container").hide();var n=$(".course-unit");n.on("click","li",function(){$(this).children("a").get(0).click()}),n.on("click","h4",function(){var n=$(this);n.siblings("ul").eq(0).slideToggle(300),n.toggleClass("open"),n.find(".fa-2x").toggleClass("fa-angle-right").toggleClass("fa-angle-down")})});
